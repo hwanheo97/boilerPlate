923769c27d26e821a66468b7e3338304729f61fc
hwanheo <38911266+hwanheo97@users.noreply.github.com>
2021-12-19 17:55:01 +0900

    update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -8,8 +8,8 @@ module.exports = function(app) {
      //proxy로 대체 가능 <= { createProxyMiddleware }
    //  target: 'http://localhost:5000',    //local
    //target: localhost:process.env.PORT || 5000,
-  //  target: 'http://3.139.239.112/process.env.PORT || 5000',    //deploy
-   target: 'https://benplate.herokuapp.com/process.env.PORT || 5000',
+  //  target: 'http://3.139.239.112/process.env.PORT || 5000',    //deploy aws okay
+   target: 'https://benplate.herokuapp.com:process.env.PORT || 5000',
       changeOrigin: true,
       
     })
