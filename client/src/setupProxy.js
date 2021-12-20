5f06d520c342475048926002ad7eaa20c459c1c6
hwanheo <38911266+hwanheo97@users.noreply.github.com>
2021-12-20 15:56:26 +0900

    update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
      //proxy로 대체 가능 <= { createProxyMiddleware }
-    target: 'http://localhost:5000',    //local
-   //target: 'https//localhost:process.env.PORT || 5000',
+    //target: 'http://localhost:5000',    //local
+   target: 'https//localhost/process.env.PORT || 5000',
   //  target: 'http://3.139.239.112/process.env.PORT || 5000',    //deploy aws okay
    //target: 'https://benplate.herokuapp.com/process.env.PORT || 5000',
       changeOrigin: true,
