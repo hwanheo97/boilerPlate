1fac6135c9412192ecd5e3e838b2ae945c43ba8b
hwanheo <38911266+hwanheo97@users.noreply.github.com>
2021-12-21 16:03:43 +0900

    update

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -23,12 +23,12 @@ function LandingPage(props) {
                 }
             })
     }
-
+   
     return (
         <div style={{ display:'flex', justifyContent:'center',alignItems: 'center',
                         width:'100%', height:'100vh'}}>
             <h2 >LandingPage</h2>
-        
+          
                 <button style={{marginLeft: "10px"}} onClick={onClickHandler}>
                     로그아웃
                 </button>
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     //target: 'http://localhost:5000',    //local
    //target: 'https//localhost:process.env.PORT || 5000',
   //  target: 'http://3.139.239.112/process.env.PORT || 5000',    //deploy aws okay
-   target: 'https://benplate.herokuapp.com:process.env.PORT || 8080',
+   target: 'http://benplate.herokuapp.com:process.env.PORT || 5000',
       changeOrigin: true,
       
     })
